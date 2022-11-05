0d2bd565be707341f9f73e15ad734a3361826fd9
plexxi <plexxious@outlook.com>
2022-11-05 09:20:19 -0500

    removed useless class variable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/states/lib/RobotState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/states/lib/RobotState.java
@@ -4,7 +4,6 @@ package org.firstinspires.ftc.teamcode.teleop.states.lib;
 import org.firstinspires.ftc.teamcode.teleop.controllers.RobotController;
 
 public abstract class RobotState {
-    StateMachine machine;
     public abstract void enter(RobotController controller);
     public abstract RobotState loop(RobotController controller) throws NoNewStateException;
     public abstract RobotState exit(RobotController controller) throws NoNewStateException;
