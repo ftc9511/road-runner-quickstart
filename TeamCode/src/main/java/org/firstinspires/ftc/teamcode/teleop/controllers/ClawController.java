0172e341a14633678832e237a5596a0dc2fc863f
plexxi <plexxious@outlook.com>
2022-11-05 09:17:35 -0500

    refactored type of claw servo positions and added state

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/controllers/ClawController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/controllers/ClawController.java
@@ -4,35 +4,36 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class ClawController {
-    private Servo left;
-    private Servo right;
+    private final Servo left;
+    private final Servo right;
 
-    private int LEFT_OPEN = 0;
-    private double LEFT_CLOSED = 0.4;
-    private int RIGHT_OPEN = 1;
-    private double RIGHT_CLOSED = 0.6;
+    private final double LEFT_OPEN = 0;
+    private final double LEFT_CLOSED = 0.4;
+    private final double RIGHT_OPEN = 1;
+    private final double RIGHT_CLOSED = 0.6;
+    public ClawState state;
+
+    public enum ClawState {
+        GRAB,
+        OPEN
+    }
 
 
     public ClawController(HardwareMap map) {
         this.left = map.get(Servo.class, "left_claw");
         this.right = map.get(Servo.class, "right_claw");
+        this.expand();
     }
 
     public void expand() {
         this.left.setPosition(LEFT_OPEN);
         this.right.setPosition(RIGHT_OPEN);
+        this.state = ClawState.OPEN;
     }
 
     public void grab() {
-        this.left.setPosition(LEFT_OPEN);
-        this.right.setPosition(RIGHT_OPEN);
-    }
-
-    public boolean isExtended() {
-        return this.left.getPosition() == this.LEFT_OPEN && this.right.getPosition() == this.RIGHT_OPEN;
-    }
-
-    public boolean isRetracted() {
-        return this.left.getPosition() == this.LEFT_CLOSED && this.right.getPosition() == this.LEFT_CLOSED;
+        this.left.setPosition(LEFT_CLOSED);
+        this.right.setPosition(RIGHT_CLOSED);
+        this.state = ClawState.GRAB;
     }
 }
