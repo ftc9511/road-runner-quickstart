4529fc8397aee1efd7eb9df054a5613c8202ced7
plexxi <plexxious@outlook.com>
2022-11-05 10:55:05 -0500

    added clone helper

--- git_clone_helper.py
+++ git_clone_helper.py
@@ -11,10 +11,10 @@ else:
     existing_library_path = input("Enter absolute path to existing code: ")
     quiet = False
 
-if os.listdir("./TeamCode") and not quiet:
-    overwrite = True if input("TeamCode folder already exists, proceed? (y/n) ").lower() == "y" else False
+if os.listdir("./TeamCode") and not quiet:  # Make sure that you actually want to overwrite the existing source directory
+    overwrite = True if input("TeamCode/src folder already exists, proceed? (y/n) ").lower() == "y" else False
 
 if overwrite:
     os.rmdir("TeamCode")
-os.system(f"git clone {CODE_REPO_9511_URL}")
-os.system(f"mv ./TeamCode {existing_library_path}/TeamCode/")
+os.system(f"git clone {CODE_REPO_9511_URL} > /dev/null")
+os.system(f"mv ./TeamCode {existing_library_path}/TeamCode/src > /dev/null")
