545465fe610fca271f05db53c8e11b6aa13ea634
plexxi <plexxious@outlook.com>
2022-11-05 11:37:57 -0500

    added controller for arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/controllers/ArmController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/controllers/ArmController.java
@@ -6,7 +6,29 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class ArmController {
     private final Servo arm;
 
+    public enum ArmState {
+        RAISED,
+        LOWERED
+    }
+
+    private double RAISED = 0;
+    private double LOWERED = 0;
+    public ArmState state;
+
     public ArmController(HardwareMap map) {
         this.arm = map.get(Servo.class, "arm");
+        this.state = ArmState.LOWERED;
+        this.lower();
     }
+
+    public void lower() {
+        this.arm.setPosition(this.LOWERED);
+        this.state = ArmState.LOWERED;
+    }
+
+    public void raise() {
+        this.arm.setPosition(this.RAISED);
+        this.state = ArmState.RAISED;
+    }
+
 }
