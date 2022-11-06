449c01aed7c16825098d661ca0904d4e383d64ce
hanoopgit <akp4hs@gmail.com>
2022-11-06 13:49:18 -0600

    added vision files, auton framework, and gradle implementations

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/drive/AutonomousOpmodes/BaseAutonomous9511.java
@@ -0,0 +1,193 @@
+package org.firstinspires.ftc.teamcode.auton.drive.AutonomousOpmodes;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.auton.Vision.AprilTagDetectionPipeline;
+import org.firstinspires.ftc.teamcode.auton.drive.SampleMecanumDrive;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class BaseAutonomous9511 extends LinearOpMode {
+
+    protected SampleMecanumDrive drive;
+
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+    static final double FEET_PER_METER = 3.28084;
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+    double tagsize = 0.166;
+    int LEFT = 1;
+    int MIDDLE = 2;
+    int RIGHT = 3;
+    AprilTagDetection tagOfInterest = null;
+
+    protected DcMotorEx lift;
+    protected Servo shoulder;
+    protected Servo leftGrabber;
+    protected Servo rightGrabber;
+
+    protected double RAISED = 0.2688;
+    protected double LOWERED = 0.2688;
+    protected int EXTEND_TICKS = -2250;
+    protected double LEFT_OPEN = 0;
+    protected double LEFT_CLOSED = 0.4;
+    protected double RIGHT_OPEN = 1;
+    protected double RIGHT_CLOSED = 0.6;
+
+    protected void initRobot(){
+        initState("Starting");
+
+        initState("Drivetrain");
+        drive = new SampleMecanumDrive(hardwareMap);
+
+        initState("Servos");
+        shoulder = hardwareMap.get(Servo.class, "shoulder");
+        leftGrabber = hardwareMap.get(Servo.class, "left");
+        rightGrabber = hardwareMap.get(Servo.class, "right");
+
+        initState("Motors");
+        lift = hardwareMap.get(DcMotorEx.class, "lift");
+
+        initState("Vision");
+        initVision();
+
+        initState("Done");
+    }
+
+    protected void initState(String step) {
+        telemetry.addData("Initializing", step);
+        telemetry.update();
+    }
+
+    protected void initVision(){
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+    }
+
+    protected void waitForStartVisionPipeline(){
+        while (!isStarted() && !isStopRequested())
+        {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+            if(currentDetections.size() != 0)
+            {
+                boolean tagFound = false;
+
+                for(AprilTagDetection tag : currentDetections)
+                {
+                    if(tag.id == LEFT || tag.id == MIDDLE || tag.id == RIGHT)
+                    {
+                        tagOfInterest = tag;
+                        tagFound = true;
+                        break;
+                    }
+                }
+
+                if(tagFound)
+                {
+                    telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+                    tagToTelemetry(tagOfInterest);
+                }
+                else
+                {
+                    telemetry.addLine("Don't see tag of interest :(");
+
+                    if(tagOfInterest == null)
+                    {
+                        telemetry.addLine("(The tag has never been seen)");
+                    }
+                    else
+                    {
+                        telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                        tagToTelemetry(tagOfInterest);
+                    }
+                }
+
+            }
+            else
+            {
+                telemetry.addLine("Don't see tag of interest :(");
+
+                if(tagOfInterest == null)
+                {
+                    telemetry.addLine("(The tag has never been seen)");
+                }
+                else
+                {
+                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                    tagToTelemetry(tagOfInterest);
+                }
+
+            }
+
+            telemetry.update();
+            sleep(20);
+        }
+    }
+
+    protected void telemetryVision(){
+        if(tagOfInterest != null)
+        {
+            telemetry.addLine("Tag snapshot:\n");
+            tagToTelemetry(tagOfInterest);
+            telemetry.update();
+        }
+        else
+        {
+            telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
+            telemetry.update();
+        }
+    }
+
+    void tagToTelemetry(AprilTagDetection detection)
+    {
+        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
+        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
+        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/drive/AutonomousOpmodes/RedAuton1.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.auton.drive.AutonomousOpmodes;
+
+public class RedAuton1 extends BaseAutonomous9511{
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initRobot();
+
+        waitForStartVisionPipeline();
+
+        telemetryVision();
+
+        if(tagOfInterest.id == LEFT || tagOfInterest == null){
+            //LEFT case autonomous
+        } else if (tagOfInterest.id == MIDDLE){
+            //MIDDLE case autonomous
+        } else{
+            //RIGHT case autonomous
+        }
+    }
+}
