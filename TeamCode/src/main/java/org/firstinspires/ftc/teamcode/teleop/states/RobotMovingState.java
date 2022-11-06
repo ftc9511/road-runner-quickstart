71833f32f2b4945d28e2933c090b232245ba5e9a
plexxi <plexxious@outlook.com>
2022-11-06 07:02:45 -0600

    updated robot moving state and fixed null pointer exception

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/states/RobotMovingState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/states/RobotMovingState.java
@@ -5,24 +5,26 @@ import org.firstinspires.ftc.teamcode.teleop.states.lib.NoNewStateException;
 import org.firstinspires.ftc.teamcode.teleop.states.lib.RobotState;
 
 public class RobotMovingState extends RobotState {
-    public void enter(RobotController controller) {}
+    public void enter(RobotController controller) {
+        this.controller = controller;
+    }
     public RobotState loop() throws NoNewStateException {
         // left goes up down left right
         // right rotates
 
-        double x = -controller.gamepad.left_stick_y;
-        double y = controller.gamepad.left_stick_x;
-        double z = controller.gamepad.right_stick_x;
+        double x = -this.controller.gamepad.left_stick_y;
+        double y = this.controller.gamepad.left_stick_x;
+        double z = this.controller.gamepad.right_stick_x;
 
-        controller.motors.mecanumDrive(y, x, z);
+        this.controller.motors.mecanumDrive(y, x, z);
 
-        if (controller.gamepad.left_trigger > 0) {
+        if (this.controller.gamepad.left_trigger > 0) {
             return new ClawGrabState();
         }
-        if (controller.gamepad.dpad_up) {
+        if (this.controller.gamepad.dpad_up) {
             return new RobotLinearSlideExtend();
         }
-        if (controller.gamepad.dpad_down) {
+        if (this.controller.gamepad.dpad_down) {
             return new RobotLinearSlideRetract();
         }
         return this;
