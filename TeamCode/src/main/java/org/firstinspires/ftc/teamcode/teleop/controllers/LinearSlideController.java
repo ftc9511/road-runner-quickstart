253e4e575e172231873e5bb7e950dca9be246d6a
plexxi <plexxious@outlook.com>
2022-11-06 08:47:25 -0600

    fixed linear slide motor configuration

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/controllers/LinearSlideController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/controllers/LinearSlideController.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.teleop.controllers;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 
@@ -12,22 +13,24 @@ public class LinearSlideController {
     public LinearSlideController(HardwareMap map) {
         this.motor = map.get(DcMotor.class, "linear_slide");
         this.RETRACT_TICKS = this.motor.getCurrentPosition();
+        this.motor.setDirection(DcMotorSimple.Direction.REVERSE);
         this.motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        this.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
     public void extend() {
         this.motor.setTargetPosition(this.EXTEND_TICKS);
-        this.motor.setPower(1);
+        this.motor.setPower(0.8);
     }
     public void retract() {
         this.motor.setTargetPosition(this.RETRACT_TICKS);
-        this.motor.setPower(1);
+        this.motor.setPower(0.8);
     }
 
     public boolean isExtended() {
-        return this.motor.getCurrentPosition() == this.EXTEND_TICKS;
+        return this.EXTEND_TICKS < this.motor.getCurrentPosition() && this.motor.getCurrentPosition() < this.EXTEND_TICKS+200;
     }
 
     public boolean isRetracted() {
-        return this.motor.getCurrentPosition() == this.RETRACT_TICKS;
+        return this.RETRACT_TICKS < this.motor.getCurrentPosition() && this.motor.getCurrentPosition() < this.RETRACT_TICKS+30;
     }
 }
