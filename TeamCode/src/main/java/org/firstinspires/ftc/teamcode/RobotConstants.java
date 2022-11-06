6317755bde2bb2a3d7374e5b18430e4dcaba5081
plexxi <plexxious@outlook.com>
2022-11-06 13:37:33 -0600

    added docstring to robot constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 
 public final class RobotConstants {
+    // Houses all of the positions/values for robot hardware
     public static double ARM_RAISED = 0.2688;
     public static double ARM_LOWERED = 0.0988;
     public static int LINEAR_SLIDE_EXTEND_TICKS = -2250;
