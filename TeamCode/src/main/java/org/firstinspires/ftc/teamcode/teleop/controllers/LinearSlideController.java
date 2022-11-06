367a88fbe3af42d716bbbb59f98359c290d1c9c4
plexxi <plexxious@outlook.com>
2022-11-06 07:16:24 -0600

    fixed a logic error in linear slide control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/controllers/LinearSlideController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/controllers/LinearSlideController.java
@@ -7,19 +7,20 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class LinearSlideController {
     private DcMotor motor;
     private final int EXTEND_TICKS = -2250;
-    private final int RETRACT_TICKS = 5;
+    private int RETRACT_TICKS;
 
     public LinearSlideController(HardwareMap map) {
         this.motor = map.get(DcMotor.class, "linear_slide");
+        this.RETRACT_TICKS = this.motor.getCurrentPosition();
         this.motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
     public void extend() {
         this.motor.setTargetPosition(this.EXTEND_TICKS);
-        this.motor.setPower(0.75);
+        this.motor.setPower(1);
     }
     public void retract() {
         this.motor.setTargetPosition(this.RETRACT_TICKS);
-        this.motor.setPower(0.75);
+        this.motor.setPower(1);
     }
 
     public boolean isExtended() {
