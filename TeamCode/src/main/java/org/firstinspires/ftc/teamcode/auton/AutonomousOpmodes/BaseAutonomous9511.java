0ad6166ddb40cf424f7e6c9160b49f7d1fbb07bb
hanoopgit <akp4hs@gmail.com>
2022-11-14 15:56:41 -0600

    Updated BaseAutonomous9511.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonomousOpmodes/BaseAutonomous9511.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/AutonomousOpmodes/BaseAutonomous9511.java
@@ -66,6 +66,7 @@ public abstract class BaseAutonomous9511 extends LinearOpMode {
         shoulder = hardwareMap.get(Servo.class, "shoulder");
         leftGrabber = hardwareMap.get(Servo.class, "left");
         rightGrabber = hardwareMap.get(Servo.class, "right");
+        grabbingPosition();
 
         initState("Motors");
         lift = hardwareMap.get(DcMotorEx.class, "lift");
@@ -241,5 +242,18 @@ public abstract class BaseAutonomous9511 extends LinearOpMode {
         } else{
             PARKINGTARGET = 3;
         }
+    }
+
+    protected void grabbingPosition(){
+        shoulder.setPosition(LOWERED);
+        sleep(2000);
+        rightGrabber.setPosition(RIGHT_CLOSED);
+        leftGrabber.setPosition(LEFT_CLOSED);
+    }
+
+    protected void scoringPosition(boolean drop){
+        shoulder.setPosition(RAISED);
+        rightGrabber.setPosition(drop?RIGHT_OPEN:RIGHT_CLOSED);
+        leftGrabber.setPosition(drop?LEFT_OPEN:LEFT_CLOSED);
     }
 }
