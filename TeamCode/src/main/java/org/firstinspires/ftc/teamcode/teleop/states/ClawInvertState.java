864c35bf40577a02e21fcf42a89ef47bb3d92ddc
plexxi <plexxious@outlook.com>
2022-11-19 10:21:58 -0600

    refactored states to allow for inversion

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/states/ClawInvertState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/states/ClawInvertState.java
@@ -1,22 +1,23 @@
 package org.firstinspires.ftc.teamcode.teleop.states;
 
+import org.firstinspires.ftc.teamcode.teleop.controllers.ClawController;
 import org.firstinspires.ftc.teamcode.teleop.controllers.RobotController;
 import org.firstinspires.ftc.teamcode.teleop.states.lib.NoNewStateException;
 import org.firstinspires.ftc.teamcode.teleop.states.lib.RobotState;
-import org.firstinspires.ftc.teamcode.teleop.controllers.ClawController.ClawState;
 
-public class ClawGrabState extends RobotState {
-
+public class ClawInvertState extends RobotState {
     @Override
     public void enter(RobotController controller) {
         this.controller = controller;
-        this.controller.claw.grab();
+        this.controller.claw.expand();
     }
 
     @Override
     public RobotState loop() throws NoNewStateException {
-        if (!(this.controller.claw.state == ClawState.OPEN)) {
-            return this;
+        if (this.controller.claw.state == ClawController.ClawState.OPEN) {
+            this.controller.claw.grab();
+        } else {
+            this.controller.claw.expand();
         }
         return this.exit();
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/states/RobotArmInvertState.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.teleop.states;
+
+import org.firstinspires.ftc.teamcode.teleop.controllers.RobotController;
+import org.firstinspires.ftc.teamcode.teleop.states.lib.NoNewStateException;
+import org.firstinspires.ftc.teamcode.teleop.states.lib.RobotState;
+
+public class RobotArmInvertState extends RobotState {
+
+    @Override
+    public void enter(RobotController controller) {
+        switch (this.controller.arm.state) {
+            case RAISED:
+                this.controller.arm.lower();
+                break;
+            case LOWERED:
+                this.controller.arm.raise();
+        }
+    }
+    @Override
+    public RobotState loop() throws NoNewStateException {
+        return this.exit();
+    }
+    @Override
+    public RobotState exit() throws NoNewStateException {
+        throw new NoNewStateException();
+    };
+}
