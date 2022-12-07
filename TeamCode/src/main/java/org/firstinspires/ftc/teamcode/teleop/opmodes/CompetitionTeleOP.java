187a10d836230e0b5cbd639b7ee0dcf8382f8d73
plexxi <plexxious@outlook.com>
2022-12-06 18:45:55 -0600

    Added "Competition TeleOP" for ease of debugging during competition

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/opmodes/CompetitionTeleOP.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.teleop.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.teleop.base.BaseRobot;
+import org.firstinspires.ftc.teamcode.teleop.controllers.RobotController;
+
+@TeleOp(name="[No-FSM] 9511 Movement TeleOP")
+public class CompetitionTeleOP extends BaseRobot {
+    private Gamepad gamepad;
+    private RobotController controller;
+
+    @Override
+    public void init() {
+        this.gamepad = gamepad1;
+        this.controller = new RobotController(hardwareMap, telemetry, this.gamepad);
+    }
+
+    @Override
+    public void loop() {
+        // Calculate drivetrain values and apply them
+        double x = -this.controller.gamepad.left_stick_y;
+        double y = this.controller.gamepad.left_stick_x;
+        double z = this.controller.gamepad.right_stick_x;
+
+        this.controller.motors.mecanumDrive(y, x, z);
+
+        // Extremity movements
+        if (this.controller.gamepad.left_trigger > 0) {
+            this.controller.claw.invert();
+        }
+
+        if (this.controller.gamepad.dpad_up) {
+            this.controller.slide.extend();
+        }
+        if (this.controller.gamepad.dpad_down) {
+            this.controller.slide.retract();
+        }
+
+        if (this.controller.gamepad.a) {
+            this.controller.arm.invert();
+        }
+
+    }
+}
