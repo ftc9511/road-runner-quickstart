24d55a76777bc7ee36877666860f9a79435856bc
plexxi <plexxious@outlook.com>
2022-12-09 18:42:28 -0600

    fixed claw open and close constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
@@ -7,9 +7,9 @@ public final class RobotConstants {
     public static double ARM_LOWERED = 0.0988;
     public static int LINEAR_SLIDE_EXTEND_TICKS = -2250;
     public static double CLAW_LEFT_OPEN = 1;
-    public static double CLAW_LEFT_CLOSED = 0.6;
+    public static double CLAW_LEFT_CLOSED = 0;
     public static double CLAW_RIGHT_OPEN = 0;
-    public static double CLAW_RIGHT_CLOSED = 0.6;
+    public static double CLAW_RIGHT_CLOSED = 1;
 
     private RobotConstants() {}
 }
