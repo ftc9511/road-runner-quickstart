bfe515cefb51ab78dcb1540275521d887102287c
plexxi <plexxious@outlook.com>
2022-12-23 18:31:42 -0600

    updated hardware constants for claw and linear slide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
@@ -5,11 +5,11 @@ public final class RobotConstants {
     // Houses all of the positions/values for robot hardware
     public static double ARM_RAISED = 0.2688;
     public static double ARM_LOWERED = 0.0988;
-    public static int LINEAR_SLIDE_EXTEND_TICKS = -2250;
-    public static double CLAW_LEFT_OPEN = 1;
-    public static double CLAW_LEFT_CLOSED = 0;
+    public static int LINEAR_SLIDE_EXTEND_TICKS = 1950;
+    public static double CLAW_LEFT_OPEN = 0;
+    public static double CLAW_LEFT_CLOSED = 0.5;
     public static double CLAW_RIGHT_OPEN = 0;
-    public static double CLAW_RIGHT_CLOSED = 1;
+    public static double CLAW_RIGHT_CLOSED = 0.5;
 
     private RobotConstants() {}
 }
