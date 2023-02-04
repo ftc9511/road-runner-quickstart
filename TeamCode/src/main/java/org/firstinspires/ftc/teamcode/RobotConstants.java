d837114d5b44e368e6efdcc9fc8f1023d253c7b6
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-02-04 17:19:54 -0600

    updated servo constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
@@ -1,15 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
 
-public final class RobotConstants {
+public class RobotConstants {
     // Houses all of the positions/values for robot hardware
     public static double ARM_RAISED = 0.2688;
     public static double ARM_LOWERED = 0.0988;
     public static int LINEAR_SLIDE_EXTEND_TICKS = -1950;
     public static double CLAW_LEFT_CLOSED = 0.63;
-    public static double CLAW_LEFT_OPEN = 0.47;
+    public static double CLAW_LEFT_OPEN = 0.037;
     public static double CLAW_RIGHT_CLOSED = 0.18;
-    public static double CLAW_RIGHT_OPEN = 0.34;
+    public static double CLAW_RIGHT_OPEN = 0.7;
 
     private RobotConstants() {}
 }
