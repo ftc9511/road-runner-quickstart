9998a8c4b5f04f6d4bfc39f29be15ce59e4e2d29
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-02-11 17:31:39 -0600

    added controller that allows for easy linear slide control

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/controllers/JunctionLevelController.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.teleop.controllers;
+
+import java.util.List;
+
+public class JunctionLevelController {
+    private LinearSlideController controller;
+    private List<Integer> junctionTicks;
+    private int current;
+
+    public JunctionLevelController(LinearSlideController controller) {
+        this.controller = controller;
+        this.current = junctionTicks.get(0);
+    }
+
+    public void next() {
+        if (this.current == junctionTicks.get(junctionTicks.size() - 1)) {
+            return; // We have nowhere else to go
+        } else {
+            this.current = junctionTicks.get(junctionTicks.indexOf(this.current) + 1);
+            this.controller.toPosition(this.current);
+        }
+    }
+
+    public void back() {
+        if (this.current == junctionTicks.get(0)) {
+            return; // We have nowhere else to go
+        } else {
+            this.current = junctionTicks.get(junctionTicks.indexOf(this.current) - 1);
+            this.controller.toPosition(this.current);
+        }
+    }
+}
