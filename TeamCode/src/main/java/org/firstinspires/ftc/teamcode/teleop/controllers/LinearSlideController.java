d12bccf14826e0c06f78d16a6261b89a4a09dcde
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-02-11 16:52:54 -0600

    removed obsolete code and attempted fix for linear slide falling down

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/controllers/LinearSlideController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/controllers/LinearSlideController.java
@@ -14,35 +14,14 @@ public class LinearSlideController {
         this.motor = map.get(DcMotor.class, "lift");
         this.RETRACT_TICKS = this.motor.getCurrentPosition();
         this.motor.setDirection(DcMotorSimple.Direction.REVERSE);
-        this.motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        this.motor.setTargetPosition(this.RETRACT_TICKS);
+        this.motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         this.motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
     public void extend() {
         this.motor.setTargetPosition(RobotConstants.LINEAR_SLIDE_EXTEND_TICKS);
-        this.motor.setPower(0.8);
     }
     public void retract() {
         this.motor.setTargetPosition(this.RETRACT_TICKS);
-        this.motor.setPower(0.8);
-    }
-
-    public void manualControl(double direction) {
-        this.motor.setPower(direction * 0.8);
-    }
-
-    public boolean isExtended() {
-        return RobotConstants.LINEAR_SLIDE_EXTEND_TICKS < this.motor.getCurrentPosition() && this.motor.getCurrentPosition() < RobotConstants.LINEAR_SLIDE_EXTEND_TICKS+200;
-    }
-
-    public boolean isRetracted() {
-        return this.RETRACT_TICKS < this.motor.getCurrentPosition() && this.motor.getCurrentPosition() < this.RETRACT_TICKS+30;
-    }
-
-    public void invert() {
-        if (this.isExtended()) {
-            this.retract();
-        } else if (this.isRetracted()) {
-            this.extend();
-        } // Otherwise, we are currently moving and thus should not change state
     }
 }
