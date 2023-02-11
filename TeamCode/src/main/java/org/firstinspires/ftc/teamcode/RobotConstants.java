ed6116a5cf034d50dd344ddda3d9eff0fe7327c8
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-02-11 11:01:03 -0600

    changed servo limits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
@@ -3,13 +3,11 @@ package org.firstinspires.ftc.teamcode;
 
 public class RobotConstants {
     // Houses all of the positions/values for robot hardware
-    public static double ARM_RAISED = 0.2688;
-    public static double ARM_LOWERED = 0.0988;
     public static int LINEAR_SLIDE_EXTEND_TICKS = -1950;
-    public static double CLAW_LEFT_CLOSED = 0.63;
-    public static double CLAW_LEFT_OPEN = 0.037;
-    public static double CLAW_RIGHT_CLOSED = 0.18;
-    public static double CLAW_RIGHT_OPEN = 0.7;
+    public static double CLAW_LEFT_CLOSED = 0.697;
+    public static double CLAW_LEFT_OPEN = 0.457;
+    public static double CLAW_RIGHT_CLOSED = 0.211;
+    public static double CLAW_RIGHT_OPEN = 0.398;
 
     private RobotConstants() {}
 }
