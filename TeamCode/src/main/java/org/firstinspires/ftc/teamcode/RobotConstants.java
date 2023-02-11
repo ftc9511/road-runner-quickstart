c8456759ab53d9274933ddfff8a57758d7019aa9
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-02-11 16:51:00 -0600

    changed linear slide tick amount

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 
 public class RobotConstants {
     // Houses all of the positions/values for robot hardware
-    public static int LINEAR_SLIDE_EXTEND_TICKS = -1950;
+    public static int LINEAR_SLIDE_EXTEND_TICKS = 3600;
     public static double CLAW_LEFT_CLOSED = 0.697;
     public static double CLAW_LEFT_OPEN = 0.457;
     public static double CLAW_RIGHT_CLOSED = 0.211;
