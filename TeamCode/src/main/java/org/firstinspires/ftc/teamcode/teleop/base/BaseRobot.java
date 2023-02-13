17edba56b51448147c4b598180411268374cf14b
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-02-13 16:30:53 -0600

    removed obsolete fsm code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/base/BaseRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/base/BaseRobot.java
@@ -4,11 +4,9 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.teleop.controllers.RobotController;
-import org.firstinspires.ftc.teamcode.teleop.states.lib.StateMachine;
 
 
 public abstract class BaseRobot extends OpMode {
-    public StateMachine machine;
     public RobotController controller;
 
     public abstract void init();
