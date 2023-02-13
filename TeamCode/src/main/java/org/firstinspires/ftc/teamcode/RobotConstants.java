63c3cb53c5ee5f61634a0116f07101b5ca4a55c9
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-02-13 16:31:59 -0600

    removed useless code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 
+import java.util.List;
+
 public class RobotConstants {
     // Houses all of the positions/values for robot hardware
     public static int LINEAR_SLIDE_EXTEND_TICKS = 3600;
@@ -8,6 +10,5 @@ public class RobotConstants {
     public static double CLAW_LEFT_OPEN = 0.457;
     public static double CLAW_RIGHT_CLOSED = 0.211;
     public static double CLAW_RIGHT_OPEN = 0.398;
-
     private RobotConstants() {}
 }
