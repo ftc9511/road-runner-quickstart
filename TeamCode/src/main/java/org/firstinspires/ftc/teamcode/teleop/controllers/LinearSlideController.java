0879ae65f149b0ea67d14f244e865dbe50b22aad
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-02-15 16:34:11 -0600

    added toPosition method that eases the slide into a position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/controllers/LinearSlideController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/controllers/LinearSlideController.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode.teleop.controllers;
 
+import static java.lang.Math.abs;
+import static java.lang.Math.max;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.teamcode.RobotConstants;
 
 
@@ -10,6 +13,7 @@ public class LinearSlideController {
 
     private DcMotor motor;
     public int RETRACT_TICKS;
+    public int targetTicks;
 
     public LinearSlideController(HardwareMap map) {
         this.motor = map.get(DcMotor.class, "lift");
@@ -18,7 +22,7 @@ public class LinearSlideController {
         this.RETRACT_TICKS = this.motor.getCurrentPosition();
         this.motor.setTargetPosition(this.RETRACT_TICKS);
         this.motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        this.motor.setPower(0.40);
+        this.targetTicks = this.RETRACT_TICKS;
     }
     public void fullExtend() {
         this.motor.setTargetPosition(RobotConstants.LINEAR_SLIDE_EXTEND_TICKS);
@@ -27,9 +31,14 @@ public class LinearSlideController {
         this.motor.setTargetPosition(this.RETRACT_TICKS);
     }
 
-
-    public void toRelativePosition(int pos) {
-        this.motor.setTargetPosition(pos);
+    public void manualControl(double direction) {
+        this.motor.setPower(direction * 0.8);
     }
 
+    public void toPosition(int amount) {
+        this.targetTicks += amount;
+        int liftPosition = Range.clip(this.targetTicks, this.RETRACT_TICKS, this.RETRACT_TICKS + RobotConstants.LINEAR_SLIDE_EXTEND_TICKS);
+        this.motor.setTargetPosition(liftPosition);
+        this.motor.setPower(max(0.2, abs(liftPosition - this.motor.getCurrentPosition()) / 500.0));
+    }
 }
