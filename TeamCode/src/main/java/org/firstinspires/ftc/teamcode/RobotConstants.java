448ee775b1ed80f298f57d8f1e483a76a120e7d3
thewarsawpakt <88908312+thewarsawpakt@users.noreply.github.com>
2023-02-17 16:18:34 -0600

    added comments and linear slide speed control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConstants.java
@@ -1,14 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
 
-import java.util.List;
-
 public class RobotConstants {
     // Houses all of the positions/values for robot hardware
-    public static int LINEAR_SLIDE_EXTEND_TICKS = 4000;
+    public static int LINEAR_SLIDE_EXTEND_TICKS = 4000; // Maximum extend length for the slide
     public static double CLAW_LEFT_CLOSED = 0.697;
     public static double CLAW_LEFT_OPEN = 0.457;
     public static double CLAW_RIGHT_CLOSED = 0.211;
     public static double CLAW_RIGHT_OPEN = 0.398;
+    public static double LINEAR_SLIDE_VELOCITY_MULTIPLIER = 0.8; // Speed control for linear slide
     private RobotConstants() {}
 }
